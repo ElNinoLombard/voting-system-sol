6d513bbbe896b5fe58b56dbf7167392abc9ab84e
Maxime Lombard <75131538+ElNinoLombard@users.noreply.github.com>
2024-03-22 10:41:56 +0100

    Update VotingSystem.sol

--- VotingSystem.sol
+++ VotingSystem.sol
@@ -1,1 +1,46 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract VotingSystem {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+
+    uint[] public candidateIds;
+
+    mapping(address => bool) public voters;
+
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only contract owner can perform this operation");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function registerCandidate(uint _id, string memory _name) public onlyOwner {
+        require(candidates[_id].id == 0, "Candidate already exists");
+        Candidate memory newCandidate = Candidate(_id, _name, 0);
+        candidates[_id] = newCandidate;
+        candidateIds.push(_id);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender], "You have already voted");
+        require(candidates[_candidateId].id != 0, "Candidate does not exist");
+        candidates[_candidateId].voteCount++;
+        voters[msg.sender] = true;
+    }
+
+    function getTotalCandidates() public view returns(uint) {
+        return candidateIds.length;
+    }
+}
